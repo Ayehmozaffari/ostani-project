52066288ead4fd1126e0ce074ebc1260439c0ea9
₳ɣēɦ <ayehmozaffari1402@gmail.com>
2023-09-05 15:41:46 +0330

    sgfhsg

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-document.querySelector('.shopping-btn').addEventListener('Click' , validation)
-document.querySelector('.shopping-btn1').addEventListener('Click' , validation)
-document.querySelector('.shopping-btn2').addEventListener('Click' , validation)
-document.querySelector('.shopping-btn3').addEventListener('Click' , validation)
-function validation(e){
-  if(e.target.Click){
-    alert("you have to select product");
-  }
-}
+// document.querySelector('.shopping-btn').addEventListener('Click' , validation)
+// document.querySelector('.shopping-btn1').addEventListener('Click' , validation)
+// document.querySelector('.shopping-btn2').addEventListener('Click' , validation)
+// document.querySelector('.shopping-btn3').addEventListener('Click' , validation)
+// function validation(e){
+//   if(e.target.Click){
+//     alert("you have to select product");
+//   }
+// }
